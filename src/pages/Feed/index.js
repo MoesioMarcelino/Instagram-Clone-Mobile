a8171962d7d6cef2b64d1e09fe1b3c3bedc50602
MoesioMarcelino <moesiomarcelino1@gmail.com>
2019-08-25 22:27:21 -0300

    infine list: lazy loading

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,23 +1,43 @@
 import React, {useState, useEffect} from 'react';
 import {View, FlatList} from 'react-native';
 
-import {Post, Header, Avatar, Name, PostImage, Description} from './styles';
+import {
+  Post,
+  Header,
+  Avatar,
+  Name,
+  PostImage,
+  Description,
+  Loading,
+} from './styles';
 
 export default function Feed() {
   const [feed, setFeed] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  async function loadPage(pageNumber = page) {
+    if (total && pageNumber > total) return;
+
+    setLoading(true);
+
+    const response = await fetch(
+      `http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`,
+    );
+
+    const data = await response.json();
+    const totalItem = response.headers.get('X-Total-Count');
+
+    setTotal(Math.floor(totalItem / 5));
+
+    setFeed([...feed, ...data]);
+    setPage(pageNumber + 1);
+    setLoading(false);
+  }
 
   useEffect(() => {
-    async function loadFeed() {
-      const response = await fetch(
-        'http://localhost:3000/feed?_expand=author&_limit=5&_page=1',
-      );
-
-      const data = await response.json();
-
-      setFeed(data);
-    }
-
-    loadFeed();
+    loadPage();
   }, []);
 
   return (
@@ -25,6 +45,9 @@ export default function Feed() {
       <FlatList
         data={feed}
         keyExtractor={post => String(post.id)}
+        onEndReached={() => loadPage()}
+        onEndReachedThreshold={0.1}
+        ListFooterComponent={loading && <Loading />}
         renderItem={({item}) => (
           <Post>
             <Header>
